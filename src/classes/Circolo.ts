2c279a31d36ae7e0ae6e5ace6fcd664b81673aca
Matteo Gastaldello <matteo.gastaldello-1@studenti.unitn.it>
2023-05-10 18:49:34 +0200

    aggiunto class OrarioGiornaliero, da rivedere

--- src/classes/Circolo.ts
+++ src/classes/Circolo.ts
@@ -5,14 +5,60 @@ type DocumentoSocietario = { //TODO: meglio di così
     documento: string
 }
 
-export enum TipoCampo{
-    INTERNO = "INTERNO",
-    ESTERNO = "ESTERNO"
+export enum TipoCampo {
+    Interno = "Interno",
+    Esterno = "Esterno"
+}
+
+export enum GiornoSettimana {
+    Lunedi,
+    Martedi,
+    Mercoledi,
+    Giovedi,
+    Venerdi,
+    Sabato,
+    Domenica
 }
 
 export class Campo {
+    @prop({ required: true })
     public id: number
+
+    @prop({required: true})
     public tipologia: TipoCampo
+}
+export class OrarioGiornaliero {
+    @prop({ required: true })
+    public giorno: GiornoSettimana
+
+    @prop({ required: true })
+    public isAperto: boolean
+    //ho dei dubbi
+    @prop({ 
+        required: true, 
+        set: (hour: number, minutes: number) => {
+            var data = new Date();
+            data.setHours(hour);
+            data.setMinutes(minutes);
+            return data;
+        },
+        get: (apertura: Date) => apertura.toLocaleTimeString()
+    })
+    private orarioApertura: Date
+
+    @prop({ 
+        required: true, 
+        set: (hour: number, minutes: number) => {
+            var data = new Date();
+            data.setHours(hour);
+            data.setMinutes(minutes);
+            return data;
+        },
+        get: (apertura: Date) => apertura.toLocaleTimeString()
+    })
+    @prop({ required: true })
+    private orarioChiusura: Date
+
 }
 
 export class Circolo extends Utente {
@@ -32,7 +78,7 @@ export class Circolo extends Utente {
     @prop()
     public paymentOnboarding?: boolean
 
-    @prop({required: true})
+    @prop({ required: true })
     public validato: boolean = false
 
     @prop()
@@ -57,14 +103,11 @@ export class Circolo extends Utente {
     }
 
     getPrezzoSlotOrarioAffiliato(): number | undefined {
-        if(this.prezzoSlotOrario){
+        if (this.prezzoSlotOrario) {
             return ((100 - this.scontoAffiliazione) * this.prezzoSlotOrario) / 100
         }
         return
     }
-
-
-
 }
 
 export const CircoloModel = getDiscriminatorModelForClass(UtenteModel, Circolo, TipoAccount.Circolo);
