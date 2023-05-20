6901972e885ed55ed8cab3f402f30e04641cf1e4
Luca <Luca.checchin97@gmail.com>
2023-05-20 03:52:56 +0200

    api_partite_CRUD_raw

--- src/routes/partite/partiteRouter.ts
+++ src/routes/partite/partiteRouter.ts
@@ -6,70 +6,29 @@ import { Giocatore,GiocatoreModel } from '../../classes/Giocatore'
 import { Partita, PartitaModel } from '../../classes/Partita'
 import { checkTokenCircolo } from "../../middleware/tokenChecker";
 import { logger } from "../../utils/logging";
+import {createPartita, readPartita , readAllPartite, deletePartita, updatePartita} from '../../controllers/partite'
 
 
 const router = Router();
 
 //get all matches
-router.get("/", async (req : Request, res : Response ) => {
-    /*
-    return all partite
-    */
-   try{
-        const partite = await PartitaModel.find();
-        res.status(200).json({partite})
-        console.log(partite)
-   }catch(err){
-        res.status(500).json()
-
-   }
-})
+router.get("/", readAllPartite); 
 
 
 //get a single match
-router.get("/:id",async (req : Request , res : Response) =>{
-    try{
-        
-        const partita= await PartitaModel.findById(req.params.id)
-        res.status(200).json(partita)
-        console.log(partita, req.params)
-
-
-
-    }catch(err){
-        res.status(500).json()
-        console.log(err)
-        
-
-    }
-})
+router.get("/:PartitaId",readPartita);
 
 //add partita
-router.post("/",async (req : Request, res : Response)=>{
-    try{
+router.post("/", createPartita);
+    
 
-        var partita = await new PartitaModel(req.body).save()
-        res.status(200).json(partita)
-        
-
-    }catch(err){
-        res.status(404).json()
-    }
-})
 //delete partita
-router.delete("/:id", async (req: Request, res : Response) =>{
-    try{
-        const partita= await PartitaModel.findByIdAndDelete(req.params.id)
-        res.status(200).json(partita)
+router.delete("/:PartitaId", deletePartita);
 
-        console.log(partita)
-        
-    }
-    catch(err){
-        res.status(500).json({status: 500, message : "Internal Error"})
 
-    }
-})
+//
+router.patch("/:PartitaId",updatePartita)
+
 
 
 
