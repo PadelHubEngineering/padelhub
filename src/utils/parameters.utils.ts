db515fcffecae663f150fe92beaf75ddfa3f1b7a
SabriVinco <vincosabri@gmail.com>
2023-05-24 17:29:39 +0200

    Utils per controlli email, telefono e password

--- src/utils/parameters.utils.ts
+++ src/utils/parameters.utils.ts
@@ -24,3 +24,57 @@ export function controlloData(res: Response, value: any, error_message: string, 
 
     return new Date(value);
 }
+
+export function controlloEmail(res:Response, value: any, error_message: string, value_name?: string){
+
+    //Se non è una stringa e se è vuota
+    if( !controlloStringa(res, value, false, error_message, value_name)) return null
+
+    //Se l'email non rispetta il regex
+    if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value))){
+        let msg = `${error_message}: ${ value_name || "L'email inserita" } non e' valida`;
+
+        sendHTTPResponse(res, 400, false, msg)
+        return null
+    }
+    
+    //Controllo se l'email esiste già
+    //Controllo se il nome del circolo esiste già
+    return value as string
+
+}
+
+export function controlloTelefono(res:Response, value: any, error_message: string, value_name?: string){
+
+    //Se non è una stringa e se è vuota 
+    if( !controlloStringa(res, value, false, error_message, value_name)) return null
+
+    //Se non rispetta il formato di un telephone number
+    if(!(/^(3[0-9]{8,9})|(0{1}[1-9]{1,3})[\s|.|-]?(\d{4,})$/.test(value))){
+        let msg = `${error_message}: ${ value_name || "numero di telefono inserito" } non valido`;
+
+        sendHTTPResponse(res, 400, false, msg)
+        return null        
+    }
+
+    return value as string;
+
+}
+
+
+export function controlloPassword(res:Response, value: any, error_message: string, value_name?: string){
+
+    //Se non è una stringa e se è vuota 
+    if( !controlloStringa(res, value, false, error_message, value_name)) return null
+
+    //Controllo se rispetta il regex
+    if( !(/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/).test(value)){
+        let msg = `${error_message}: ${ value_name || "La password inserita" } non valida`;
+
+        sendHTTPResponse(res, 400, false, msg)
+        return null  
+    }
+
+    return value as string;
+
+}
