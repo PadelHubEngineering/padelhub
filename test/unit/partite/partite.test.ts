3e64615e16cca865a039b9376a47e8ee056de42d
Matteo Gastaldello <matteo.gastaldello-1@studenti.unitn.it>
2023-05-29 12:05:41 +0200

    aggiunto altri test

--- test/unit/partite/partite.test.ts
+++ test/unit/partite/partite.test.ts
@@ -6,9 +6,10 @@ import { PartitaModel } from '../../../src/classes/Partita';
 import { jest } from '@jest/globals';
 import { response } from 'express';
 import { ResolveTypegooseNameError } from '@typegoose/typegoose/lib/internal/errors';
+import { GiocatoreModel } from '../../../src/classes/Giocatore';
+import { CircoloModel } from '../../../src/classes/Circolo';
 
 describe('GET /api/v1/partite/:idPartita', () => {
-    jest.useFakeTimers();
 
     beforeAll(() => {
         PartitaModel.findById = jest.fn().mockImplementation((params) => {
@@ -25,30 +26,11 @@ describe('GET /api/v1/partite/:idPartita', () => {
                     categoria_min: 1,
                     giocatori: ["64679d72d7391e02188e77e1"],
                     circolo: "64679d72d7391e02188e77e4",
-                    orario: new Date(0, 0)
+                    orario: new Date("1899-12-31T23:00:00.000Z")
                 } as never)
             }
         }) as any;
-        // PartitaModel.findById = jest.fn().mockReturnValueOnce({
-        //     populate: (jest.fn() as jest.Mock).mockResolvedValueOnce({
-        //         id_partita: "64679d72d7391e02188e77e0",
-        //         isChiusa: false,
-        //         categoria_max: 5,
-        //         categoria_min: 1,
-        //         giocatori: ["64679d72d7391e02188e77e1"],
-        //         circolo: "64679d72d7391e02188e77e4",
-        //         orario: new Date(0, 0)
-        //     } as never)
-        // }) as any;
     });
-
-    // beforeEach(async () => {
-    //     jest.useFakeTimers();
-    // });
-
-    // afterEach(async () => {
-    //     jest.useRealTimers();
-    // });
     afterAll(async () => {
         PartitaModel.findById = jest.fn() as any;
     })
@@ -66,10 +48,10 @@ describe('GET /api/v1/partite/:idPartita', () => {
     )
 
     test('GET /api/v1/partite/:idPartita con id valido', async () => {
-        const response = await request(app).get('/api/v1/partite/64679d72d7391e02188e77e0').set('x-access-token', token).send()
-        expect(response.status).toBe(200)
-        expect(response.body).toHaveProperty("success", true)
-        expect(response.body).toHaveProperty("payload", {
+        const resp = await request(app).get('/api/v1/partite/64679d72d7391e02188e77e0').set('x-access-token', token).send()
+        expect(resp.status).toBe(200)
+        expect(resp.body).toHaveProperty("success", true)
+        expect(resp.body).toHaveProperty("payload", {
             id_partita: "64679d72d7391e02188e77e0",
             isChiusa: false,
             categoria_max: 5,
@@ -80,10 +62,10 @@ describe('GET /api/v1/partite/:idPartita', () => {
         } as never)
     });
     test('GET /api/v1/partite/:idPartita con id valido ma partita non presente', async () => {
-        const response = await request(app).get('/api/v1/partite/64679d72d7391e02188e77e1').set('x-access-token', token).send()
-        expect(response.status).toBe(404)
-        expect(response.body).toHaveProperty("success", false)
-        expect(response.body).toHaveProperty("message", "Partita inesistente")
+        const resp = await request(app).get('/api/v1/partite/64679d72d7391e02188e77e1').set('x-access-token', token).send()
+        expect(resp.status).toBe(404)
+        expect(resp.body).toHaveProperty("success", false)
+        expect(resp.body).toHaveProperty("message", "Partita inesistente")
     });
     test('GET /api/v1/partite/:idPartita con id non valido', async () => {
         request(app).get('/api/v1/partite/10')
@@ -97,5 +79,95 @@ describe('GET /api/v1/partite/:idPartita', () => {
                 }
             });
     });
+});
+
+describe("GET /api/v1/partite/", () => {
+    const retValue: any = [{
+        id_partita: "64679d72d7391e02188e77e0",
+        isChiusa: false,
+        categoria_max: 5,
+        categoria_min: 1,
+        giocatori: ["64679d72d7391e02188e77e1"],
+        circolo: "64679d72d7391e02188e77e4",
+        orario: "1899-12-31T23:00:00.000Z"
+    }, 
+    {
+        id_partita: "64679d72d7391e02188e77e2",
+        isChiusa: false,
+        categoria_max: 5,
+        categoria_min: 1,
+        giocatori: ["64679d72d7391e02188e77e1"],
+        circolo: "64679d72d7391e02188e77e4",
+        orario: "1899-12-31T23:00:00.000Z"
+    }]
+
+    beforeAll(() => {
+        PartitaModel.find = jest.fn().mockImplementation((params) => {
+            return {
+                populate: (jest.fn() as jest.Mock).mockResolvedValueOnce(retValue as never)
+            }
+        }) as any;
+        CircoloModel.findOne = jest.fn().mockImplementation((params) => {
+            if((<any>params).email != "test@circolo.com")
+                return null;
+            return retValue;
+        }) as any;
+    });
+    afterAll(async () => {
+        PartitaModel.find = jest.fn() as any;
+    })
+
+    var tokenCircolo = jwt.sign(
+        {
+            tipoAccount: "Circolo",
+            email: "test@circolo.com",
+            nome: "testCircolo"
+        },
+        process.env.SUPER_SECRET!,
+        {
+            expiresIn: process.env.DEFAULT_EXPIRATION_PERIOD || "2d"
+        }
+    )
+    var tokenGiocatore = jwt.sign(
+        {
+            tipoAccount: "Giocatore",
+            email: "test@giocatore.com",
+            nome: "testGiocatore"
+        },
+        process.env.SUPER_SECRET!,
+        {
+            expiresIn: process.env.DEFAULT_EXPIRATION_PERIOD || "2d"
+        }
+    )
+
+    test('GET /api/v1/partite/ con token Giocatore', async () => {
+        const resp = await request(app).get('/api/v1/partite/').set('x-access-token', tokenGiocatore).send()
+        expect(resp.status).toBe(200)
+        expect(resp.body).toHaveProperty("success", true)
+        expect(resp.body).toHaveProperty("payload", retValue)
+    })
+    test('GET /api/v1/partite/ con token Circolo con mail valida', async () => {
+        const resp = await request(app).get('/api/v1/partite/').set('x-access-token', tokenCircolo).send()
+        expect(resp.status).toBe(200)
+        expect(resp.body).toHaveProperty("success", true)
+        expect(resp.body).toHaveProperty("payload", retValue)
+    })
+    test('GET /api/v1/partite/ con token Circolo con mail non valida', async () => {
+        var tokenCircoloNotValid = jwt.sign(
+            {
+                tipoAccount: "Circolo",
+                email: "invalid@circolo.com",
+                nome: "testCircolo"
+            },
+            process.env.SUPER_SECRET!,
+            {
+                expiresIn: process.env.DEFAULT_EXPIRATION_PERIOD || "2d"
+            }
+        )
+        const resp = await request(app).get('/api/v1/partite/').set('x-access-token', tokenCircoloNotValid).send()
+        expect(resp.status).toBe(500)
+        expect(resp.body).toHaveProperty("success", false)
+        expect(resp.body).toHaveProperty("message", "[server] Errore interno")
+    })
 });
 
