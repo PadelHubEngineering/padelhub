703e14ce0e0af75226113f6d73e716e57851b0cf
Luca <Luca.checchin97@gmail.com>
2023-06-11 10:21:48 +0200

    spostamento_ update_campo

--- src/routes/pagamenti/pagamentiRouter.ts
+++ src/routes/pagamenti/pagamentiRouter.ts
@@ -44,6 +44,50 @@ router.post('/webhook', async (req: Request, res: Response) => {
                 const partita = await PartitaModel.findById(prenotazione?.partita)
                 if(partita){
                     partita.aggiungi_player(prenotazione.giocatore)
+                    if(partita.checkChiusa()){
+                            //per prendere l'id del campo
+
+    /*
+    var id_campi_prenotati : number[] = []
+    campi_prenotati.forEach(campo => id_campi_prenotati.push(campo.idCampo))
+    console.log(campi_prenotati)
+    console.log(id_campi_prenotati)
+    var campi_liberi_esterni : number[] = []
+    var campi_liberi_interni  : number[] = []
+
+
+    c.campi.forEach( (campo)=> {if(campo.tipologia==TipoCampo.Esterno){
+        campi_liberi_esterni.push(campo.id);
+        }else{campi_liberi_interni.push(campo.id)}
+    })
+    console.log(campi_liberi_interni)
+    console.log(campi_liberi_esterni) //fare la pop(dei campi uguali campi_liber.includes(campo_prenotato)) e poi restituire il primo id dell'array: idCampo= array.at[0]
+
+    */
+    //creazione prenotazione campo (SOLO DOPO ULTIMA PRENOTAZIONE DOVEééééé???)
+
+    /*
+    const prenotazione_campo= {
+        idCampo : 1, // calcolato da sopra
+        partita : partita._id,
+        circolo : circolo,
+        inizioSlot : date,
+        fineSlot : c.get_fineSlot(date).toJSDate(),
+        dataPrenotazione : DateTime.now().toJSDate()
+
+    }
+    
+    const re = await PrenotazioneCampoModel.create(prenotazione_campo)
+    if(!re){
+        sendHTTPResponse(res, 500, false, "[server] Errore interno")
+        return
+
+    }
+    
+    console.log(re)
+    */
+    
+                    }
                 }
             }
             
@@ -54,6 +98,7 @@ router.post('/webhook', async (req: Request, res: Response) => {
     }
     // Return a 200 response to acknowledge receipt of the event
     res.json({ received: true });
+    
 })
 
 
